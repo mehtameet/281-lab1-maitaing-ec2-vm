00187eb41faec04d94d3ec277ed837047fa83e4b
Meet Mehta <mehtameet007@gmaill.com>
2013-11-10 19:33:08 -0800

    delete, start, stop and restart functionality added

--- src/instance_methods.java
+++ src/instance_methods.java
@@ -1,19 +1,35 @@
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Set;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
 import com.amazonaws.services.ec2.AmazonEC2Client;
+import com.amazonaws.services.ec2.model.DescribeImagesResult;
+import com.amazonaws.services.ec2.model.DescribeInstancesResult;
+import com.amazonaws.services.ec2.model.Image;
 import com.amazonaws.services.ec2.model.Instance;
+import com.amazonaws.services.ec2.model.InstanceState;
+import com.amazonaws.services.ec2.model.Reservation;
 import com.amazonaws.services.ec2.model.RunInstancesRequest;
 import com.amazonaws.services.ec2.model.RunInstancesResult;
+import com.amazonaws.services.ec2.model.StartInstancesRequest;
+import com.amazonaws.services.ec2.model.StopInstancesRequest;
+import com.amazonaws.services.ec2.model.TerminateInstancesRequest;
 
 public class instance_methods {
 
 	AmazonEC2Client ec2;
+	List<String> instanceIds=new LinkedList<String>();
 	public instance_methods() throws IOException{
+		
+		//setting up credentials
 		AWSCredentials credentials = new PropertiesCredentials(mainClass.class.getResourceAsStream("AwsCredentials.properties"));
 		ec2 = new AmazonEC2Client(credentials);
+		
+		//setting endpoints for ec2Client
 		ec2.setEndpoint("ec2.us-west-2.amazonaws.com");
 	}
 
@@ -47,5 +63,72 @@ public class instance_methods {
 
 		}
 
+	}
+	
+	public void deleteInstance()
+	{
+
+		instanceIds.add("i-a806109c");
+		System.out.println("#8 Terminate the Instance");
+		System.out.println("instance ids are : "+instanceIds);
+        TerminateInstancesRequest tir = new TerminateInstancesRequest(instanceIds);
+        ec2.terminateInstances(tir);
+	}
+	
+	public void startInstance() throws InterruptedException
+	{
+		boolean result=false;
+		instanceIds.add("i-a806109c");
+		while(result==false)
+		{
+			try
+			{
+				StartInstancesRequest startIR = new StartInstancesRequest(instanceIds);
+		        ec2.startInstances(startIR);
+		        result=true;
+			}
+			catch(com.amazonaws.AmazonServiceException e)
+			{
+				System.out.println("amazon instance is yet not stopped, again trying to start in 15 seconds");
+				Thread.currentThread().sleep(15000);
+			}
+		}
+		
+		
+	}
+	
+	public void stopInstance()
+	{
+		instanceIds.add("i-a806109c");
+		StopInstancesRequest stopIR = new StopInstancesRequest(instanceIds);
+        ec2.stopInstances(stopIR);
+	}
+	
+	public void describeInstance()
+	{
+		 DescribeImagesResult dir = ec2.describeImages();
+         List<Image> images = dir.getImages();
+         System.out.println("You have " + images.size() + " Amazon images");
+         
+         DescribeInstancesResult describeInstancesRequest = ec2.describeInstances();
+         List<Reservation> reservations = describeInstancesRequest.getReservations();
+         Set<Instance> instances = new HashSet<Instance>();
+         // add all instances to a Set.
+         for (Reservation reservation : reservations) {
+          instances.addAll(reservation.getInstances());
+         }
+        
+         System.out.println("You have " + instances.size() + " Amazon EC2 instance(s).");
+         for (Instance ins : instances){
+        
+          // instance id
+          String instanceId = ins.getInstanceId();
+        
+          // instance state
+          InstanceState is = ins.getState();
+          System.out.println("launch time is "+ins.getLaunchTime());
+          System.out.println(ins.getKeyName()+ " "+ ins.getIamInstanceProfile()+ ins.getPlatform()+ ins.getRamdiskId());
+          System.out.println(instanceId+" "+is.getName());
+         }
 	}
 }
--- src/mainClass.java
+++ src/mainClass.java
@@ -1,4 +1,5 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 import com.amazonaws.auth.AWSCredentials;
 import com.amazonaws.auth.PropertiesCredentials;
@@ -18,8 +19,56 @@ public class mainClass {
 
 		instance_methods methods = new instance_methods();
 		
-		methods.createInstance();
 		
+		int option;
+		while(true)
+		{
+			Scanner input = new Scanner(System.in);
+			System.out.println("Enter  1. Create Instance  2. Delete Instance  3. Stop Instance   4.Start Instance   5. Restart Instance   6. List Characteristics   7.Create Ami from Instance");
+			option = input.nextInt();
+			
+			switch (option) {
+			case 1:
+				System.out.println("Create instance query fired");
+				methods.createInstance();
+				System.out.println("Instance Created");
+				break;
+
+			case 2:
+				System.out.println("Delete instance query fired");
+				methods.deleteInstance();
+				System.out.println("Instance is deleted successfully");
+				break;
+				
+			case 3:
+				System.out.println("stop instance query fired");
+				methods.stopInstance();
+				System.out.println("Instance stopped successfully");
+				break;
+				
+			case 4:
+				System.out.println("start instance query fired");
+				methods.startInstance();
+				System.out.println("Instance started successfully");
+				break;
+			
+			case 5:
+				System.out.println("restart instance query fired");
+				methods.stopInstance();
+				//start will has inbuilt try-catch block which checks if the instant is stopped perfectly or not before start
+				methods.startInstance();
+				System.out.println("Instance restarted successfully");
+				break;
+				
+			case 6:
+				System.out.println("Describing instances");
+				methods.describeInstance();
+				break;
+				
+			default:
+				break;
+			}
+		}
 		
 	}
 
